4ffc6deaca7c9242ac217d2c7aab9280ef660903
joão <juaun1981@gmail.com>
2024-06-30 09:14:18 -0300

    Implementação dos turnos de cada jogador e tratamento de excecoes

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -68,6 +69,13 @@ public class UI {
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
+	}
+
+	public static void printMatch(ChessMatch chessMatch){
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn());
+		System.out.println("Current Player: " + chessMatch.getCurrentPlayer());	
 	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
